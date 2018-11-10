0090392025ea097fc2918f3a71019d1088840c40
Barry Sheppard <barrysheppard@gmail.com>
2018-11-10 13:12:43 +0000

    Added tests for all functions

--- Q3_StudentID_tests.py
+++ Q3_StudentID_tests.py
@@ -10,8 +10,13 @@ class Q3_checker(unittest.TestCase):
         self.class_test = DocumentAnalyser(file_name)
 
     def test_DocumentAnalyser(self):
-#        self.assertTrue(self.spellChecker.check_word('zygotic'))
         self.assertEqual(2, self.class_test.calc_number_of_lines())
+        # This should include test for each method
+        self.assertEqual(8, self.class_test.calc_number_of_words())
+        self.assertEqual(33, self.class_test.calc_number_of_characters())
+        self.assertEqual(16.5, self.class_test.get_avg_chars_per_line())
+        self.assertEqual(4, self.class_test.get_avg_words_per_line())
+        self.assertEqual(4.125, self.class_test.get_avg_characters_per_word())
 
 
 if __name__ == '__main__':
